5c0a43486d9ebfff1cada6302997f151fc51e377
Vlas <vlasdergaev@gmail.com>
2023-11-04 18:37:02 +0600

    Add RandomDataReader

---
+++ BotsCommon/IO/Data/RandomDataReader.cs
@@ -0,0 +1,35 @@
+﻿namespace BotsCommon.IO.Data
+{
+    public sealed class RandomDataReader<T> : IDataReader<T>
+    {
+        private readonly IDataReader<T> _dataReader;
+
+        public RandomDataReader(IDataReader<T> dataReader)
+        {
+            _dataReader = dataReader;
+        }
+
+        public int Length => _dataReader.Length;
+        public int Index => _dataReader.Index;
+
+        public T Read()
+        {
+            var skipCount = Random.Shared.Next(0, _dataReader.Length - _dataReader.Index);
+
+            for (var i = 0; i < skipCount; i++)
+                _dataReader.Read();
+
+            return _dataReader.Read();
+        }
+
+        public void Reset()
+        {
+            _dataReader.Reset();
+        }
+
+        public void Dispose()
+        {
+            _dataReader.Dispose();
+        }
+    }
+}
