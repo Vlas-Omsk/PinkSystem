960e5ad67723087e79cbd6548542a2988b62dbac
Vlas <vlasdergaev@gmail.com>
2024-01-20 15:13:01 +0700

    Add RegexExtensions

---
+++ BotsCommon/Extensions/RegexExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BotsCommon
+{
+    public static class RegexExtensions
+    {
+        public static Group ThrowIfNotSuccuess(this Group self)
+        {
+            if (!self.Success)
+                throw new Exception($"Group {self.Index} ({self.Name}) not success");
+
+            return self;
+        }
+    }
+}
