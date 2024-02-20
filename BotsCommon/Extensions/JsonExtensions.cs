a47db783e06041026a337a7c70805ed132dc463a
Vlas <vlasdergaev@gmail.com>
2024-02-20 17:37:45 +0700

    Add json extensions

--- BotsCommon/Extensions/JsonExtensions.cs
+++ BotsCommon/Extensions/JsonExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿#nullable enable
+
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Globalization;
 using System.Text;
@@ -21,6 +23,34 @@ namespace BotsCommon
                 serializer.Serialize(writer, self);
 
             return stringWriter.ToString();
+        }
+
+        public static JToken SelectPath(this JToken self, params object[] path)
+        {
+            foreach (var name in path)
+                self = self[name] ?? throw new Exception($"Cannot find '{name}' in json by path '{string.Join('.', path)}'");
+
+            return self;
+        }
+
+        public static T ValueRequired<T>(this JToken self)
+        {
+            return self.Value<T?>() ?? throw new Exception($"Value cannot be converted to type {typeof(T)}");
+        }
+
+        public static JArray AsArray(this JToken self)
+        {
+            return (JArray)self;
+        }
+
+        public static JObject AsObject(this JToken self)
+        {
+            return (JObject)self;
+        }
+
+        public static JValue AsValue(this JToken self)
+        {
+            return (JValue)self;
         }
     }
 }
