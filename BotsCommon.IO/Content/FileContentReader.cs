5c52cc913c639d1f6be79dd6eae89b30baf20a9a
Vlas <vlasdergaev@gmail.com>
2024-04-16 22:44:14 +0600

    Add Path to FileContentReader

--- BotsCommon.IO/Content/FileContentReader.cs
+++ BotsCommon.IO/Content/FileContentReader.cs
@@ -5,22 +5,22 @@ namespace BotsCommon.IO.Content
 {
     public sealed class FileContentReader : IContentReader
     {
-        private readonly string _path;
         private readonly FileInfo _info;
 
         public long? Length => _info.Length;
         public string MimeType { get; }
+        public string Path { get; }
 
         public FileContentReader(string path, string? mimeType = null)
         {
-            _path = path;
-            _info = new FileInfo(_path);
+            Path = path;
+            _info = new FileInfo(Path);
             MimeType = mimeType ?? MimeTypeMap.GetMimeType(_info.Name);
         }
 
         public Stream CreateStream()
         {
-            return File.OpenRead(_path);
+            return File.OpenRead(Path);
         }
     }
 }
