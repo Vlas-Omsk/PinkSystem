7828f4bc9ed2994c34c56d278cdf8d28c6d58dc8
Vlas <vlasdergaev@gmail.com>
2024-06-22 15:25:32 +0300

    Add BrowserInfoDataReader

---
+++ BotsCommon.Net/IO/Data/BrowserInfoDataReader.cs
@@ -0,0 +1,42 @@
+﻿using BotsCommon.IO.Data;
+
+namespace BotsCommon.Net.IO.Data
+{
+    public sealed class BrowserInfoDataReader : IDataReader<BrowserInfo>
+    {
+        private readonly IDataReader<string> _reader;
+
+        public BrowserInfoDataReader(IDataReader<string> reader)
+        {
+            _reader = reader;
+        }
+
+        public int? Length => _reader.Length;
+        public int Index => _reader.Index;
+
+        public BrowserInfo? Read()
+        {
+            var str = _reader.Read();
+
+            if (str == null)
+                return null;
+
+            var split = str.Split('|');
+
+            if (split.Length == 2)
+                return new(split[0], split[1]);
+
+            return new(split[0], null);
+        }
+
+        public void Reset()
+        {
+            _reader.Reset();
+        }
+
+        public void Dispose()
+        {
+            _reader.Dispose();
+        }
+    }
+}
