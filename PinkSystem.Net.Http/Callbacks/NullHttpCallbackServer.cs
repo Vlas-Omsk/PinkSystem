426e76acc5cff7e94fe733c42fc2c6be417b65e2
Vlas <vlasdergaev@gmail.com>
2024-08-30 22:47:49 +0300

    Add NullHttpCallbackServer

---
+++ PinkSystem.Net.Http/Callbacks/NullHttpCallbackServer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PinkSystem.Net.Http.Callbacks
+{
+    public sealed class NullHttpCallbackServer : IHttpCallbackServer
+    {
+        private sealed class NullHttpCallbackHandler : IHttpCallbackHandler
+        {
+            public bool TryCreateReceiver(string path, [NotNullWhen(true)] out IHttpCallbackReceiver? receiver)
+            {
+                receiver = null;
+                return false;
+            }
+        }
+
+        public IHttpCallbackHandler CreateHandler()
+        {
+            return new NullHttpCallbackHandler();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
