f0f55557657d95c36772594a326092a3d61160f2
Vlas <vlasdergaev@gmail.com>
2025-02-05 20:45:35 +0500

    Add CookieExtensions

---
+++ PinkSystem/Extensions/CookieExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+
+namespace PinkSystem
+{
+    public static class CookieExtensions
+    {
+        public static void MakeExpired(this Cookie self)
+        {
+            self.Expires = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1));
+        }
+    }
+}
