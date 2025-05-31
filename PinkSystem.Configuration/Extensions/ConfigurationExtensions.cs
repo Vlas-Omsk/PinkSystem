49b3bf0c6c6dbbec511eac89234b5aa993a13d2d
Vlas <vlasdergaev@gmail.com>
2025-05-31 22:55:58 +0500

    Add PinkSystem.Configuration

---
+++ PinkSystem.Configuration/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace PinkSystem.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+        public static T GetRequired<T>(this IConfiguration configuration, Action<BinderOptions>? configureOptions = null)
+        {
+            return configuration.Get<T>(configureOptions) ?? throw new NullReferenceException();
+        }
+
+        public static object GetRequired(this IConfiguration configuration, Type type, Action<BinderOptions>? configureOptions = null)
+        {
+            return configuration.Get(type, configureOptions) ?? throw new NullReferenceException();
+        }
+
+        public static T GetValueRequired<T>(this IConfiguration configuration, string key)
+        {
+            return configuration.GetRequiredSection(key).GetRequired<T>();
+        }
+
+        public static object GetValueRequired(this IConfiguration configuration, Type type, string key)
+        {
+            return configuration.GetRequiredSection(key).GetRequired(type);
+        }
+    }
+}
