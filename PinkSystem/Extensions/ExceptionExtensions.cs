ac9e2c7b98604b1542a3dcbddd4b4bfd344f5e21
Vlas <vlasdergaev@gmail.com>
2025-07-01 20:39:30 +0500

    Replace CheckAny and CheckAll with Enumerate to make it more generic

--- PinkSystem/Extensions/ExceptionExtensions.cs
+++ PinkSystem/Extensions/ExceptionExtensions.cs
@@ -1,24 +1,21 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace PinkSystem
 {
     public static class ExceptionExtensions
     {
-        public static bool CheckAny(this Exception self, Func<Exception, bool> func)
+        public static IEnumerable<Exception> Enumerate(this Exception self)
         {
             if (self is AggregateException aggregateException)
-                return aggregateException.InnerExceptions.Any(x => CheckAny(x, func));
+            {
+                foreach (var ex in aggregateException.InnerExceptions)
+                {
+                    yield return ex;
+                }
+            }
 
-            return func(self);
-        }
-
-        public static bool CheckAll(this Exception self, Func<Exception, bool> func)
-        {
-            if (self is AggregateException aggregateException)
-                return aggregateException.InnerExceptions.All(x => CheckAll(x, func));
-
-            return func(self);
+            yield return self;
         }
     }
 }
