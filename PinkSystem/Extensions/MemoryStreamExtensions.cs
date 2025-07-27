e724367dfc0e520a3fe21b2e2499ec3409d4dd44
Vlas <vlasdergaev@gmail.com>
2025-07-28 02:02:31 +0500

    Add MemoryStream.ToReadOnlySpan extension

--- PinkSystem/Extensions/MemoryStreamExtensions.cs
+++ PinkSystem/Extensions/MemoryStreamExtensions.cs
@@ -19,5 +19,19 @@ namespace PinkSystem
                 (int)self.Length
             );
         }
+
+        public static ReadOnlySpan<byte> ToReadOnlySpan(this MemoryStream self)
+        {
+            var accessor = new ObjectAccessor(self, typeof(MemoryStream));
+
+            var buffer = (byte[])accessor.GetField("_buffer")!;
+            var origin = (int)accessor.GetField("_origin")!;
+
+            return new ReadOnlySpan<byte>(
+                buffer,
+                origin,
+                (int)self.Length
+            );
+        }
     }
 }
